a0f097693a76c4491d57b5173d4db3b48e59d525
Kaan Arslan <115875320+kaan-ders@users.noreply.github.com>
2023-01-15 14:11:27 +0300

    javascript

---
+++ page.js
@@ -0,0 +1,108 @@
+//functional
+//dinamik - typed olmaması
+
+/*var sayi1 = 10;
+
+if (sayi1 === 10) {
+	alert('ahmet');
+}
+else
+	alert('fatma'); //sonuc
+
+var deneme = sayi1 == 10 ? "ahmet" : "fatma"; //ahmet
+
+switch (deneme) {
+	case '10': alert('ahmet'); break;
+	case '20': alert('ayşe'); break;
+	default: alert("hiçbiri");
+}
+
+for (var i = 0; i < sayi1; i++) {
+	alert(i);
+}
+
+while (sayi1 == "10") {
+
+}
+
+do {
+
+} while (sayi1 === 10);*/
+
+function isimYazdir() {
+	alert("ayşe");
+}
+
+function renkDegistir() {
+	var element = document.getElementById('dvKutu');
+	element.classList.add('yesil');
+}
+
+function renkSil() {
+	var element = document.getElementById('dvKutu');
+	element.classList.remove('yesil');
+}
+
+/*var liste = ['asda', 123];
+//liste[2] = 'ahmet "asdasd"';
+liste.push('ahmet "asdasd"');
+
+var obje = {
+	adi: 'Ayşe',
+	soyadi: 'Sarı',
+	yasi: 35,
+	il: 'Ankara',
+	ilce: "Cankaya",
+	maas: 5000.40,
+	departman: ['Satış', 'Muhasebe']
+};
+
+obje.telefon = "2131231" / 2;
+
+alert(obje.maas);*/
+
+//alert("Merhaba");
+
+//var sayi1 = 10;
+//alert(sayi1);
+
+//sayi1 = "ahmet";
+//alert(sayi1);
+
+//merhaba("ahmet");
+//merhaba(21412314);
+//merhaba(['asda', 'asdas', 'asdas']);
+//merhaba({deneme: 'sada'});
+
+//function merhaba(test) {
+//	alert(test);
+//}
+
+//alert(test());
+
+/*function test() {
+	
+}*/
+
+/*void merhaba(int a, char[] b)
+{
+	int sayi1 = 10;
+	sayi1 = "ahmet";
+
+	return;
+}*/
+
+/*alert(test(0));
+
+function test(p1) {
+	if (p1 > 3)
+		return "ahmet";
+	else if(p1 < 1)
+		return 8;
+}*/
+
+/*const test2 = function (p1) {
+	return 3;
+}
+
+alert(test2());*/
